abc889deb96ac0d9df76cdb8bd4aee9a1228b516
Victor Bravim <victorrafaelbravim@gmail.com>
2024-05-11 14:09:41 -0300

    Mudança

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <NavBar />
+    <div class="container mx-auto p-4">
+      <p>Conteúdo da página...</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavBar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Estilos globais ou específicos da App */
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav class="bg-gray-800 p-4">
+      <div class="container mx-auto flex justify-between items-center">
+        <div class="text-white text-lg font-semibold">Meu Site</div>
+        <ul class="flex space-x-4">
+          <li><a href="#" class="text-white hover:text-gray-300">Home</a></li>
+          <li><a href="#" class="text-white hover:text-gray-300">Sobre</a></li>
+          <li><a href="#" class="text-white hover:text-gray-300">Contato</a></li>
+        </ul>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NavBar'
+  }
+  </script>
+  
