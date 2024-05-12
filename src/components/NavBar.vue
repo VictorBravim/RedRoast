2f7d8f0b7dc13b3c0e43d76cdf9449cd58cc9a8c
Victor Bravim <victorrafaelbravim@gmail.com>
2024-05-12 18:41:06 -0300

    Mudança

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,8 +4,7 @@
       <div class="text-white text-2xl font-semibold">Red Roast</div>
       <img src="@/assets/logo.png" alt="Logo" class="h-20">
       <ul class="flex space-x-4">
-        <li><a href="#" class="text-white text-xl hover:text-gray-300">HOME</a></li>
-        <li><a href="#" class="text-white text-xl hover:text-gray-300">SOBRE</a></li>
+        <li><a href="#" class="text-white text-xl hover:text-gray-300">MEAT FRESH</a></li>
       </ul>
     </div>
   </nav>
--- src/components/ProductSection.vue
+++ src/components/ProductSection.vue
@@ -1,50 +1,50 @@
 <template>
     <div class="bg-gray-custom h-screen flex justify-center items-center flex-col py-8">
-      <h2 class="text-3xl font-semibold text-center mb-8">Nossos Produtos</h2>
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
-        <div v-for="(product, index) in products" :key="index" class="flex flex-col justify-center items-center bg-gray-custom2 p-4 px-16 shadow-md">
-          <img :src="product.image" alt="Produto" class="w-full h-40 object-cover mb-4">
-          <h3 class="text-lg font-semibold mb-2">{{ product.name }}</h3>
-          <p class="text-gray-600 text-sm mb-4">{{ product.description }}</p>
-          <button class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4">
-            Comprar
-          </button>
+        <h2 class="text-white text-5xl font-semibold text-center mb-12">Nossos Produtos</h2>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 px-12">
+            <div v-for="(product, index) in products" :key="index"
+                class="flex flex-col justify-center items-center bg-gray-custom2 p-4 px-8 shadow-md">
+                <img :src="product.image" alt="Produto" class="w-full h-full mb-4">
+                <h3 class="text-white text-2xl font-semibold mb-2">{{ product.name }}</h3>
+                <p class="text-gray-600 text-xl mb-4">{{ product.description }}</p>
+                <button class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-12">
+                    Comprar
+                </button>
+            </div>
         </div>
-      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'ProductSection',
-    data() {
-      return {
-        products: [
-          {
-            name: 'Produto 1',
-            description: 'Descrição do Produto 1',
-            image: '@/assets/product1.jpg'
-          },
-          {
-            name: 'Produto 2',
-            description: 'Descrição do Produto 2',
-            image: '@/assets/product2.jpg'
-          },
-          {
-            name: 'Produto 3',
-            description: 'Descrição do Produto 3',
-            image: '@/assets/product3.jpg'
-          },
-          {
-            name: 'Produto 4',
-            description: 'Descrição do Produto 4',
-            image: '@/assets/product4.jpg'
-          }
-        ]
-      };
-    }
-  }
-  </script>
+</template>
 
-<style scoped>
-</style>
+<script>
+export default {
+  name: 'ProductSection',
+  data() {
+    return {
+      products: [
+        {
+          name: 'Tomahawk',
+          description: 'Steak on old wooden board',
+          image: require('@/assets/tomahawk.png'), // Importe a imagem usando require()
+        },
+        {
+          name: 'Grilled Sirloin',
+          description: 'Marbled perfection juicy',
+          image: require('@/assets/grilledsirloin.png'),
+        },
+        {
+          name: 'Raw Pork',
+          description: 'Chop steak',
+          image: require('@/assets/rawpork.png'),
+        },
+        {
+          name: 'Fresh Raw',
+          description: 'Red meats green garlics',
+          image: require('@/assets/freshraw.png'),
+        }
+      ]
+    };
+  }
+}
+</script>
+
+<style scoped></style>
