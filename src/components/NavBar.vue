4f08d50347927da171c874e790f7058c27ca1273
Victor Bravim <victorrafaelbravim@gmail.com>
2024-05-12 13:57:53 -0300

    Mudança

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,11 @@
 <template>
   <nav class="bg-trasparent p-4 fixed w-full top-0 z-50">
     <div class="container mx-auto flex justify-between items-center">
-      <div class="text-white text-lg font-semibold">Meu Site</div>
+      <div class="text-white text-2xl font-semibold">RedRoast</div>
       <ul class="flex space-x-4">
-        <li><a href="#" class="text-white hover:text-gray-300">Home</a></li>
-        <li><a href="#" class="text-white hover:text-gray-300">Sobre</a></li>
-        <li><a href="#" class="text-white hover:text-gray-300">Contato</a></li>
+        <li><a href="#" class="text-white text-xl hover:text-gray-300">Home</a></li>
+        <li><a href="#" class="text-white text-xl hover:text-gray-300">Sobre</a></li>
+        <li><a href="#" class="text-white text-xl hover:text-gray-300">Contato</a></li>
       </ul>
     </div>
   </nav>
